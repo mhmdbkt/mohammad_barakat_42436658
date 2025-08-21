168e4d3bbae5a903ce0205cf38033ab24cdf1a04
MoeBarakat <156135054+mhmdbkt@users.noreply.github.com>
2025-08-21 01:11:38 +0000

    Add Django project

--- backend/flask_app/app.py
+++ backend/flask_app/app.py
@@ -1,7 +1,13 @@
-from flask import flask, jsonify
+from flask import Flask, jsonify
+
+# create flask app
 app = Flask(__name__)
+
+# simple test route
 @app.get("/api/hello")
 def hello():
-return jsonify({"message":"hello from flask!"})
-if __name__=="__name__";
-app.run(host="0.0.0.0", port=5000)
+    return jsonify({"message": "Hello from Flask!"})
+
+# run the app
+if __name__ == "__main__":   # ✅ must have double underscores
+    app.run(host="0.0.0.0", port=5000, debug=True)
